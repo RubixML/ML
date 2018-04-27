9aea0dce2172735db4a5b255ca273fad20138253
Andrew DalPino <me@andrewdalpino.com>
2018-04-26 22:33:32 -0500

    Implemented Adaline binary classifier.

--- programs/CounterfeitDetector.php
+++ programs/CounterfeitDetector.php
@@ -2,29 +2,30 @@
 
 include dirname(__DIR__) . '/vendor/autoload.php';
 
+use Rubix\Engine\Adaline;
 use Rubix\Engine\Pipeline;
 use Rubix\Engine\Prototype;
 use Rubix\Engine\Datasets\Supervised;
-use Rubix\Engine\MultiLayerPerceptron;
 use Rubix\Engine\Reports\ConfusionMatrix;
 use Rubix\Engine\Transformers\L2Regularizer;
 use Rubix\Engine\Reports\ClassificationReport;
-use Rubix\Engine\NeuralNetwork\Optimizers\Adam;
 use League\Csv\Reader;
 
 echo '╔═════════════════════════════════════════════════════╗' . "\n";
 echo '║                                                     ║' . "\n";
-echo '║ Counterfeit Detector using Multi Layer Perceptron   ║' . "\n";
+echo '║ Counterfeit Detector using Adaline                  ║' . "\n";
 echo '║                                                     ║' . "\n";
 echo '╚═════════════════════════════════════════════════════╝' . "\n";
 
-$dataset = Reader::createFromPath(dirname(__DIR__) . '/datasets/banknotes.csv')->setDelimiter(',')->getRecords();
+echo "\n";
+
+$dataset = Reader::createFromPath(dirname(__DIR__) . '/datasets/banknotes.csv')->setDelimiter(',');
 
 $dataset = Supervised::fromIterator($dataset);
 
-list($training, $testing) = $dataset->randomize()->stratifiedSplit(0.8);
+list($training, $testing) = $dataset->randomize()->stratifiedSplit(0.80);
 
-$estimator = new Prototype(new Pipeline(new MultiLayerPerceptron($dataset->columns(), [10, 10], $dataset->labels(), 3, 10, new Adam(0.01)), [
+$estimator = new Prototype(new Pipeline(new Adaline(4, 10, 3),[
     new L2Regularizer(),
 ]), [
     new ConfusionMatrix($dataset->labels()),
---
+++ src/Adaline.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Rubix\Engine;
+
+use Rubix\Engine\NeuralNetwork\Bias;
+use Rubix\Engine\Datasets\Supervised;
+use Rubix\Engine\NeuralNetwork\Input;
+use Rubix\Engine\NeuralNetwork\Neuron;
+use Rubix\Engine\NeuralNetwork\Synapse;
+use Rubix\Engine\Persisters\Persistable;
+use Rubix\Engine\NeuralNetwork\Optimizers\Adam;
+use Rubix\Engine\NeuralNetwork\Optimizers\Optimizer;
+use Rubix\Engine\NeuralNetwork\ActivationFunctions\SoftSign;
+use InvalidArgumentException;
+
+class Adaline extends Neuron implements Estimator, Classifier, Persistable
+{
+    /**
+     * The fixed number of training epochs. i.e. the number of times to iterate
+     * over the entire training set.
+     *
+     * @var int
+     */
+    protected $epochs;
+
+    /**
+     * The number of training samples to consider per iteration of gradient descent.
+     *
+     * @var int
+     */
+    protected $batchSize;
+
+    /**
+     * The gradient descent optimizer.
+     *
+     * @var \Rubix\Engine\NeuralNetwork\Optimizers\Optimizer
+     */
+    protected $optimizer;
+
+    /**
+     * The function that determines this neuron's output activation.
+     *
+     * @var \Rubix\Engine\NeuralNetwork\ActivationFunctions\ActivationFunction
+     */
+    protected $activationFunction;
+
+    /**
+     * The actual labels of the binary class outcomes.
+     *
+     * @var array
+     */
+    protected $labels = [
+        //
+    ];
+
+    /**
+     * @param  int  $epochs
+     * @param  int  $batchSize
+     * @param  \Rubix\Engine\NeuralNetwork\Optimizers\Optimizer  $optimizer
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    public function __construct(int $inputs, int $epochs = 10, int $batchSize = 10, Optimizer $optimizer = null)
+    {
+        if ($inputs < 1) {
+            throw new InvalidArgumentException('The number of inputs must be greater than 0.');
+        }
+
+        if ($epochs < 1) {
+            throw new InvalidArgumentException('Epoch parameter must be an integer greater than 0.');
+        }
+
+        if ($batchSize < 1) {
+            throw new InvalidArgumentException('Batch size cannot be less than 1.');
+        }
+
+        if (!isset($optimizer)) {
+            $optimizer = new Adam();
+        }
+
+        $this->epochs = $epochs;
+        $this->batchSize = $batchSize;
+        $this->optimizer = $optimizer;
+        $this->activationFunction = new SoftSign();
+
+        for ($i = 0; $i < $inputs; $i++) {
+            $this->connect(new Synapse(new Input()));
+        }
+
+        $this->connect(new Synapse(new Bias()));
+    }
+
+    /**
+     * Return the weight paramters of the neuron.
+     *
+     * @return array
+     */
+    public function weights() : array
+    {
+        return array_map(function ($synapse) {
+            return $synapse->weight();
+        }, $this->synapses);
+    }
+
+    /**
+     * Perform mini-batch gradient descent with given optimizer over the training
+     * set and update the input weights accordingly.
+     *
+     * @param  \Rubix\Engine\Datasets\Supervised  $dataset
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    public function train(Supervised $dataset) : void
+    {
+        $labels = $dataset->labels();
+
+        if (count($labels) !== 2) {
+            throw new InvalidArgumentException('The number of unique outcomes must be exactly 2, ' . (string) count($labels) . ' found.');
+        }
+
+        if (in_array(self::CATEGORICAL, $dataset->columnTypes())) {
+            throw new InvalidArgumentException('This estimator only works with continuous samples.');
+        }
+
+        $this->labels = [1 => $labels[0], -1 => $labels[1]];
+
+        $this->zap();
+
+        for ($epoch = 0; $epoch < $this->epochs; $epoch++) {
+            foreach ($this->generateMiniBatches(clone $dataset) as $batch) {
+                $outcomes = $batch->outcomes();
+                $sigmas = array_fill(0, count($this->synapses), 0.0);
+                $delta = 0.0;
+
+                foreach ($batch as $row => $sample) {
+                    $activation = $this->feed($sample);
+
+                    $gradient = $this->activationFunction->differentiate($activation[0], $activation[1]);
+
+                    $output = $activation[1] > 0 ? 1 : -1;
+
+                    $expected = $this->labels[$output] === $outcomes[$row] ? $output : -$output;
+
+                    $delta += $gradient * ($expected - $activation[1]);
+
+                    foreach ($this->synapses as $i => $synapse) {
+                        $sigmas[$i] += $delta * $synapse->neuron()->output();
+                    }
+                }
+
+                foreach ($this->synapses as $i => $synapse) {
+                    $this->optimizer->step($synapse, $sigmas[$i]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Read the activation of the neuron and make a prediction.
+     *
+     * @param  array  $sample
+     * @return \Rubix\Engine\Prediction
+     */
+    public function predict(array $sample) : Prediction
+    {
+        $activation = $this->feed($sample);
+
+        return new Prediction($this->labels[$activation[1] > 0 ? 1 : -1], [
+            'activation' => $activation[1],
+        ]);
+    }
+
+    /**
+     * @param  array  $sample
+     * @return array
+     */
+    public function feed(array $sample) : array
+    {
+        $column = 0;
+        $z = 0.0;
+
+        foreach ($this->synapses as $synapse) {
+            $neuron = $synapse->neuron();
+
+            if ($neuron instanceof Input) {
+                $neuron->prime($sample[$column++]);
+            }
+        }
+
+        foreach ($this->synapses as $synapse) {
+            $z += $synapse->impulse();
+        }
+
+        $computed = $this->activationFunction->compute($z);
+
+        return [$z, $computed];
+    }
+
+    /**
+     * Generate a collection of mini batches from the training data.
+     *
+     * @param  \Rubix\Engine\Datasets\Supervised  $dataset
+     * @return array
+     */
+    protected function generateMiniBatches(Supervised $dataset) : array
+    {
+        $dataset->randomize();
+
+        $batches = [];
+
+        while (!$dataset->isEmpty()) {
+            $batches[] = $dataset->take($this->batchSize);
+        }
+
+        return $batches;
+    }
+}
