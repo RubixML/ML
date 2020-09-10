a5c8a028018dc21d3c2c8614d1e7dcf71b5b0317
Andrew DalPino <me@andrewdalpino.com>
2020-09-10 04:19:15 -0500

    Fix coding style

--- src/Transformers/RecursiveFeatureEliminator.php
+++ src/Transformers/RecursiveFeatureEliminator.php
@@ -201,7 +201,7 @@ class RecursiveFeatureEliminator implements Transformer, Stateful, Verbose
 
                 $dropped[] = (int) $column;
                 unset($selected[$column]);
-                
+
                 $total += $importance;
 
                 if (count($dropped) >= $this->maxDroppedFeatures) {
