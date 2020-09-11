13597f64f5f792f12960871406418beac38f575d
Chris Simpson <chris@chrissimpson.co.uk>
2020-09-11 04:46:38 +0100

    Avoid persisting empty encoding objects (#112)
    * Avoid persisting empty encoding objects
    
    closes #111
    
    * Update exception message

--- src/Persisters/Filesystem.php
+++ src/Persisters/Filesystem.php
@@ -97,7 +97,17 @@ class Filesystem implements Persister, Stringable
             }
         }
 
-        $this->serializer->serialize($persistable)->write($this->path);
+        $encoding = $this->serializer->serialize($persistable);
+
+        if ($encoding->bytes() === 0) {
+            throw new RuntimeException("Cannot save empty file to {$this->path}");
+        }
+
+        $success = file_put_contents($this->path, $encoding->data(), LOCK_EX);
+
+        if (!$success) {
+            throw new RuntimeException('Failed to write to the filesystem.');
+        }
     }
 
     /**
@@ -116,14 +126,14 @@ class Filesystem implements Persister, Stringable
             throw new RuntimeException("File {$this->path} is not readable.");
         }
 
-        $data = new Encoding(file_get_contents($this->path) ?: '');
+        $encoding = new Encoding(file_get_contents($this->path) ?: '');
 
-        if ($data->bytes() === 0) {
+        if ($encoding->bytes() === 0) {
             throw new RuntimeException("File {$this->path} does not"
                 . ' contain any data.');
         }
 
-        return $this->serializer->unserialize($data);
+        return $this->serializer->unserialize($encoding);
     }
 
     /**
