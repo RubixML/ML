cc33ac29cfca641ca538d9e70f7627d28e78c96e
Andrew DalPino <me@andrewdalpino.com>
2021-12-12 17:15:14 -0600

    Appease Stan

--- tests/Kernels/Distance/SparseCosineTest.php
+++ tests/Kernels/Distance/SparseCosineTest.php
@@ -52,7 +52,7 @@ class SparseCosineTest extends TestCase
     }
 
     /**
-     * @return \Generator<array>
+     * @return \Generator<mixed[]>
      */
     public function computeProvider() : Generator
     {
