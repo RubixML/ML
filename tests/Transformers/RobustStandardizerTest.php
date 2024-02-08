3bb8eb32793ff9be627e9beed50e88119ccb4839
Andrew DalPino <me@andrewdalpino.com>
2024-02-08 17:39:10 -0600

    Allow deltas in units tests

--- tests/Transformers/RobustStandardizerTest.php
+++ tests/Transformers/RobustStandardizerTest.php
@@ -86,7 +86,7 @@ class RobustStandardizerTest extends TestCase
 
         $dataset->reverseApply($this->transformer);
 
-        $this->assertEquals($original, $dataset->sample(0));
+        $this->assertEqualsWithDelta($original, $dataset->sample(0), 1e-8);
     }
 
     /**
