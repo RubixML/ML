2efd1a6c14d2cd323374819a5cbe74e3547245b5
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2025-11-04 17:12:26 +0200

    388 - created new Parameter class, interface and test

---
+++ tests/NeuralNet/Parameters/ParameterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Rubix\ML\Tests\NeuralNet\Parameters;
+
+use NumPower;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use Rubix\ML\NeuralNet\Optimizers\Optimizer;
+use Rubix\ML\NeuralNet\Parameters\Parameter;
+use Rubix\ML\NeuralNet\Optimizers\Stochastic;
+use PHPUnit\Framework\TestCase;
+
+#[Group('Parameters')]
+#[CoversClass(Parameter::class)]
+class ParameterTest extends TestCase
+{
+    protected Parameter $param;
+
+    protected Optimizer $optimizer;
+
+    protected function setUp() : void
+    {
+        $this->param = new Parameter(NumPower::array([
+            [5, 4],
+            [-2, 6],
+        ]));
+
+        $this->optimizer = new Stochastic();
+    }
+
+    public function testUpdate() : void
+    {
+        $gradient = NumPower::array([
+            [2, 1],
+            [1, -2],
+        ]);
+
+        $expected = [
+            [4.98, 3.99],
+            [-2.01, 6.02],
+        ];
+
+        $this->param->update(gradient: $gradient, optimizer: $this->optimizer);
+
+        self::assertEquals($expected, $this->param->param()->asArray());
+    }
+}
