7013a569df2ce38573027d626d39cbf0b11c095d
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2025-12-19 17:58:38 +0200

    ML-392 Added `Placeholder1D` output layer with complete interface, forward/inference/backward passes, unit tests

---
+++ src/NeuralNet/Layers/Base/Contracts/Input.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rubix\ML\NeuralNet\Layers\Base\Contracts;
+
+/**
+ * Input
+ *
+ * @internal
+ *
+ * @category    Machine Learning
+ * @package     Rubix/ML
+ * @author      Andrew DalPino
+ * @author      Samuel Akopyan <leumas.a@gmail.com>
+ */
+interface Input extends Layer
+{
+    //
+}
