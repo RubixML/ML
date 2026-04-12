7e64e6ffa0e0e5467dfcbb82f7b05d80ee737b13
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-04-12 22:38:12 +0300

    ML-396 removed unused imports

--- tests/Regressors/Ridge/RidgeTest.php
+++ tests/Regressors/Ridge/RidgeTest.php
@@ -8,7 +8,6 @@ use Generator;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\Attributes\Group;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
