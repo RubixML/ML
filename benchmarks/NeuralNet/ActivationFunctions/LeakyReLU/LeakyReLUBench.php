46ba110a28ec7000569ecf5155d8666fe724f3f0
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-04-26 22:34:38 +0300

    ML-396 added LeakyReLUBench benchmark tests

---
+++ benchmarks/NeuralNet/ActivationFunctions/LeakyReLU/LeakyReLUBench.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\NeuralNet\ActivationFunctions\LeakyReLU;
+
+use NDArray;
+use NumPower;
+use Rubix\ML\NeuralNet\ActivationFunctions\LeakyReLU\LeakyReLU;
+
+/**
+ * @Groups({"ActivationFunctions"})
+ * @BeforeMethods({"setUp"})
+ */
+class LeakyReLUBench
+{
+    /**
+     * @var NDArray
+     */
+    protected NDArray $z;
+
+    /**
+     * @var NDArray
+     */
+    protected NDArray $computed;
+
+    /**
+     * @var LeakyReLU
+     */
+    protected LeakyReLU $activationFn;
+
+    public function setUp() : void
+    {
+        $this->z = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->computed = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->activationFn = new LeakyReLU();
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function activate() : void
+    {
+        $this->activationFn->activate($this->z);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function differentiate() : void
+    {
+        $this->activationFn->differentiate($this->z);
+    }
+}
