4d041f3a41b659166028c7283e77d00dccab4340
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-04-26 22:24:18 +0300

    ML-396 added GELUBench benchmark tests

---
+++ benchmarks/NeuralNet/ActivationFunctions/GELU/GELUBench.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\NeuralNet\ActivationFunctions\GELU;
+
+use NDArray;
+use NumPower;
+use Rubix\ML\NeuralNet\ActivationFunctions\GELU\GELU;
+
+/**
+ * @Groups({"ActivationFunctions"})
+ * @BeforeMethods({"setUp"})
+ */
+class GELUBench
+{
+    /**
+     * @var NDArray
+     */
+    protected NDArray $z;
+
+    /**
+     * @var NDArray
+     */
+    protected NDArray $computed;
+
+    /**
+     * @var GELU
+     */
+    protected GELU $activationFn;
+
+    public function setUp() : void
+    {
+        $this->z = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->computed = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->activationFn = new GELU();
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function compute() : void
+    {
+        $this->activationFn->activate($this->z);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function differentiate() : void
+    {
+        $this->activationFn->differentiate($this->z);
+    }
+}
