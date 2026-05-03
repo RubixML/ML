a0abae9dff3e403dae15ae39adbf417811bfb348
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-03 19:08:57 +0300

    ML-396 added SigmoidBench benchmark tests

---
+++ benchmarks/NeuralNet/ActivationFunctions/Sigmoid/SigmoidBench.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\NeuralNet\ActivationFunctions\Sigmoid;
+
+use NDArray;
+use NumPower;
+use Tensor\Matrix;
+use Rubix\ML\NeuralNet\ActivationFunctions\Sigmoid\Sigmoid;
+
+/**
+ * @Groups({"ActivationFunctions"})
+ * @BeforeMethods({"setUp"})
+ */
+class SigmoidBench
+{
+    /**
+     * @var NDArray
+     */
+    protected NDArray $z;
+
+    /**
+     * @var NDArray
+     */
+    protected NDArray $computed;
+
+    /**
+     * @var Sigmoid
+     */
+    protected Sigmoid $activationFn;
+
+    public function setUp() : void
+    {
+        $this->z = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->computed = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->activationFn = new Sigmoid();
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function activate() : void
+    {
+        $this->activationFn->activate($this->z);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function differentiate() : void
+    {
+        $this->activationFn->differentiate($this->computed);
+    }
+}
