85cf6e88544219f52e7021927ee046bec4751c78
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-03 19:12:45 +0300

    ML-396 added SiLUBench benchmark tests

---
+++ benchmarks/NeuralNet/ActivationFunctions/SiLU/SiLUBench.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\NeuralNet\ActivationFunctions\SiLU;
+
+use NDArray;
+use NumPower;
+use Rubix\ML\NeuralNet\ActivationFunctions\SiLU\SiLU;
+
+/**
+ * @Groups({"ActivationFunctions"})
+ * @BeforeMethods({"setUp"})
+ */
+class SiLUBench
+{
+    /**
+     * @var NDArray
+     */
+    protected NDArray $z;
+
+    /**
+     * @var NDArray
+     */
+    protected NDArray $computed;
+
+    /**
+     * @var SiLU
+     */
+    protected SiLU $activationFn;
+
+    public function setUp() : void
+    {
+        $this->z = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->computed = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->activationFn = new SiLU();
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function compute() : void
+    {
+        $this->activationFn->activate($this->z);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function differentiate() : void
+    {
+        $this->activationFn->differentiate($this->z);
+    }
+}
