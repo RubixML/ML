08f4935a042809c596f22afe851aa76ddf92f874
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-03 20:39:58 +0300

    ML-396 converted Blob generator to work with NDArray

---
+++ src/Datasets/Generators/Blob/Blob.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Rubix\ML\Datasets\Generators\Blob;
+
+use Rubix\ML\Datasets\Generators\Generator;
+use NDArray;
+use NumPower;
+use Rubix\ML\DataType;
+use Rubix\ML\Helpers\Stats;
+use Rubix\ML\Datasets\Dataset;
+use Rubix\ML\Datasets\Unlabeled;
+use Rubix\ML\Exceptions\InvalidArgumentException;
+
+use function count;
+use function sqrt;
+
+/**
+ * Blob
+ *
+ * A normally distributed n-dimensional blob of samples centered at a given
+ * mean vector. The standard deviation can be set for the whole blob or for each
+ * feature column independently. When a global standard deviation is used, the
+ * resulting blob will be isotropic and will converge asymptotically to a sphere.
+ *
+ * @category    Machine Learning
+ * @package     Rubix/ML
+ * @author      Andrew DalPino
+ */
+class Blob implements Generator
+{
+    /**
+     * The center vector of the blob.
+     *
+     * @var NDArray
+     */
+    protected NDArray $center;
+
+    /**
+     * The standard deviation of the blob.
+     *
+     * @var NDArray|float
+     */
+    protected $stdDev;
+
+    /**
+     * Fit a Blob generator to the samples in a dataset.
+     *
+     * @param Dataset $dataset
+     * @throws InvalidArgumentException
+     * @return self
+     */
+    public static function simulate(Dataset $dataset) : self
+    {
+        $features = $dataset->featuresByType(DataType::continuous());
+
+        if (count($features) !== $dataset->numFeatures()) {
+            throw new InvalidArgumentException('Dataset must only contain'
+                . ' continuous features.');
+        }
+
+        $means = $stdDevs = [];
+
+        foreach ($features as $values) {
+            [$mean, $variance] = Stats::meanVar($values);
+
+            $means[] = $mean;
+            $stdDevs[] = sqrt($variance);
+        }
+
+        return new self($means, $stdDevs);
+    }
+
+    /**
+     * @param (int|float)[] $center
+     * @param int|float|(int|float)[] $stdDev
+     * @throws InvalidArgumentException
+     */
+    public function __construct(array $center = [0, 0], $stdDev = 1.0)
+    {
+        if (empty($center)) {
+            throw new InvalidArgumentException('Cannot generate samples'
+                . ' with dimensionality less than 1.');
+        }
+
+        if (is_array($stdDev)) {
+            if (count($center) !== count($stdDev)) {
+                throw new InvalidArgumentException('Number of center'
+                    . ' coordinates and standard deviations must be equal.');
+            }
+
+            foreach ($stdDev as $value) {
+                if ($value < 0) {
+                    throw new InvalidArgumentException('Standard deviation'
+                        . " must be greater than 0, $value given.");
+                }
+            }
+
+            $stdDev = NumPower::array($stdDev);
+        } else {
+            if ($stdDev < 0) {
+                throw new InvalidArgumentException('Standard deviation'
+                    . " must be greater than 0, $stdDev given.");
+            }
+
+            $stdDev = (float) $stdDev;
+        }
+
+        $this->center = NumPower::array($center);
+        $this->stdDev = $stdDev;
+    }
+
+    /**
+     * Return the center coordinates of the Blob.
+     *
+     * @return list<int|float>
+     */
+    public function center() : array
+    {
+        return $this->center->toArray();
+    }
+
+    /**
+     * Return the dimensionality of the data this generates.
+     *
+     * @internal
+     *
+     * @return int<0,max>
+     */
+    public function dimensions() : int
+    {
+        return $this->center->shape()[0];
+    }
+
+    /**
+     * Generate n data points.
+     *
+     * @param int<0,max> $n
+     * @return Unlabeled
+     */
+    public function generate(int $n) : Unlabeled
+    {
+        $d = $this->dimensions();
+
+        $samples = NumPower::add(
+            NumPower::multiply(
+                NumPower::normal(size: [$n, $d], loc: 0.0, scale: 1.0),
+                $this->stdDev
+            ),
+            $this->center
+        )->toArray();
+
+        return Unlabeled::quick($samples);
+    }
+}
