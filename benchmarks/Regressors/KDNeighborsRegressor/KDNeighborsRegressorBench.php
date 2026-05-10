b002bd433cf92b7f1f0a225390744fa3b7871fdb
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-10 17:17:08 +0300

    ML-396 added KDNeighborsRegressorBench benchmark tests

---
+++ benchmarks/Regressors/KDNeighborsRegressor/KDNeighborsRegressorBench.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\Regressors\KDNeighborsRegressor;
+
+use Rubix\ML\Datasets\Labeled;
+use Rubix\ML\Regressors\KDNeighborsRegressor\KDNeighborsRegressor;
+use Rubix\ML\Datasets\Generators\Hyperplane\Hyperplane;
+
+/**
+ * @Groups({"Regressors"})
+ * @BeforeMethods({"setUp"})
+ */
+class KDNeighborsRegressorBench
+{
+    protected const int TRAINING_SIZE = 10000;
+
+    protected const int TESTING_SIZE = 10000;
+
+    protected Labeled $training;
+
+    protected Labeled $testing;
+
+    protected KDNeighborsRegressor $estimator;
+
+    public function setUp() : void
+    {
+        $generator = new Hyperplane([1, 5.5, -7, 0.01], 0.0);
+
+        $this->training = $generator->generate(self::TRAINING_SIZE);
+
+        $this->testing = $generator->generate(self::TESTING_SIZE);
+
+        $this->estimator = new KDNeighborsRegressor(5);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(5)
+     * @OutputTimeUnit("seconds", precision=3)
+     */
+    public function trainPredict() : void
+    {
+        $this->estimator->train($this->training);
+
+        $this->estimator->predict($this->testing);
+    }
+}
