abd0ba7ac129fa44dd15e4af56fb1abaaa9d44aa
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-10 15:42:36 +0300

    ML-396 added SoftsignBench benchmark tests

---
+++ benchmarks/NeuralNet/ActivationFunctions/Softsign/SoftsignBench.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\NeuralNet\ActivationFunctions\Softsign;
+
+use NDArray;
+use NumPower;
+use Rubix\ML\NeuralNet\ActivationFunctions\Softsign\Softsign;
+
+/**
+ * @Groups({"ActivationFunctions"})
+ * @BeforeMethods({"setUp"})
+ */
+class SoftsignBench
+{
+    /**
+     * @var NDArray
+     */
+    protected NDArray $z;
+
+    /**
+     * @var NDArray
+     */
+    protected NDArray $computed;
+
+    /**
+     * @var Softsign
+     */
+    protected Softsign $activationFn;
+
+    public function setUp() : void
+    {
+        $this->z = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->computed = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->activationFn = new Softsign();
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function activate() : void
+    {
+        $this->activationFn->activate($this->z);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function differentiate() : void
+    {
+        $this->activationFn->differentiate($this->computed);
+    }
+}
