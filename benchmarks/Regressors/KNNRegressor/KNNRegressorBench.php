f69abf9414a2dfae23bcc3208290a7b5f00dd44b
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-10 17:40:58 +0300

    ML-396 added KNNRegressorBench benchmark tests

---
+++ benchmarks/Regressors/KNNRegressor/KNNRegressorBench.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\Regressors\KNNRegressor;
+
+use Rubix\ML\Datasets\Labeled;
+use Rubix\ML\Regressors\KNNRegressor\KNNRegressor;
+use Rubix\ML\Datasets\Generators\Hyperplane\Hyperplane;
+
+/**
+ * @Groups({"Regressors"})
+ * @BeforeMethods({"setUp"})
+ */
+class KNNRegressorBench
+{
+    protected const int TRAINING_SIZE = 10000;
+
+    protected const int TESTING_SIZE = 10000;
+
+    protected Labeled $training;
+
+    protected Labeled $testing;
+
+    protected KNNRegressor $estimator;
+
+    public function setUp() : void
+    {
+        $generator = new Hyperplane([1, 5.5, -7, 0.01], 0.0);
+
+        $this->training = $generator->generate(self::TRAINING_SIZE);
+
+        $this->testing = $generator->generate(self::TESTING_SIZE);
+
+        $this->estimator = new KNNRegressor(5);
+    }
+
+    /**
+     * @Subject
+     * @Skip
+     * @Iterations(5)
+     * @OutputTimeUnit("seconds", precision=3)
+     */
+    public function trainPredict() : void
+    {
+        $this->estimator->train($this->training);
+
+        $this->estimator->predict($this->testing);
+    }
+}
