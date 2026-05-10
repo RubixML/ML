672ea2ea4efdb9581377fbde3a76be79ed4a8963
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-05-10 15:13:40 +0300

    ML-396 added SoftPlusBench benchmark tests

---
+++ benchmarks/NeuralNet/ActivationFunctions/SoftPlus/SoftPlusBench.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rubix\ML\Benchmarks\NeuralNet\ActivationFunctions\SoftPlus;
+
+use NDArray;
+use NumPower;
+use Rubix\ML\NeuralNet\ActivationFunctions\Softplus\Softplus;
+
+/**
+ * @Groups({"ActivationFunctions"})
+ * @BeforeMethods({"setUp"})
+ */
+class SoftPlusBench
+{
+    /**
+     * @var NDArray
+     */
+    protected NDArray $z;
+
+    /**
+     * @var NDArray
+     */
+    protected NDArray $computed;
+
+    /**
+     * @var Softplus
+     */
+    protected Softplus $activationFn;
+
+    public function setUp() : void
+    {
+        $this->z = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->computed = NumPower::uniform(size: [500, 500], low: -1.0, high: 1.0);
+
+        $this->activationFn = new Softplus();
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function activate() : void
+    {
+        $this->activationFn->activate($this->z);
+    }
+
+    /**
+     * @Subject
+     * @Iterations(3)
+     * @OutputTimeUnit("milliseconds", precision=3)
+     */
+    public function differentiate() : void
+    {
+        $this->activationFn->differentiate($this->computed);
+    }
+}
