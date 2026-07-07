a9c633aebd8b716fcd7010ab405e2db381673657
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-07-07 11:13:14 +0300

    ML-396 phpcs fix for Benchmarks\Regressors

--- benchmarks/Regressors/SVRBench.php
+++ benchmarks/Regressors/SVRBench.php
@@ -2,9 +2,9 @@
 
 namespace Rubix\ML\Benchmarks\Regressors;
 
+use Rubix\ML\Datasets\Generators\Hyperplane;
 use Rubix\ML\Datasets\Labeled;
 use Rubix\ML\Regressors\SVR;
-use Rubix\ML\Datasets\Generators\Hyperplane;
 
 /**
  * @Groups({"Regressors"})
