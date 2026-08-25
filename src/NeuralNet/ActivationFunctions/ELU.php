fdbd4bfe401daebfed18f3efb2be41fe0adec2f4
Andrew DalPino <andrewdalpino@icloud.com>
2026-08-24 21:18:17 -0500

    Fix some tests

--- src/NeuralNet/ActivationFunctions/ELU.php
+++ src/NeuralNet/ActivationFunctions/ELU.php
@@ -65,7 +65,7 @@ class ELU implements ActivationFunction
         $positiveActivation = NumPower::maximum($input, 0);
 
         $negativeMask = NumPower::minimum($input, 0);
-        
+
         $negativeActivation = NumPower::multiply(
             NumPower::expm1($negativeMask),
             $this->alpha
