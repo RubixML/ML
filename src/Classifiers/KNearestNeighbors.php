767bfdc632ff0a4857c01205ec0ec8bf10e46688
Andrew DalPino <andrewdalpino@icloud.com>
2026-08-28 15:38:12 -0500

    Initial commit

--- src/Classifiers/KNearestNeighbors.php
+++ src/Classifiers/KNearestNeighbors.php
@@ -21,9 +21,9 @@ use Rubix\ML\Specifications\LabelsAreCompatibleWithLearner;
 use Rubix\ML\Specifications\SamplesAreCompatibleWithEstimator;
 use Rubix\ML\Exceptions\InvalidArgumentException;
 use Rubix\ML\Exceptions\RuntimeException;
+use SplMaxHeap;
 
 use function Rubix\ML\argmax;
-use function array_slice;
 
 /**
  * K Nearest Neighbors
@@ -298,17 +298,37 @@ class KNearestNeighbors implements Estimator, Learner, Online, Probabilistic, Pe
      */
     protected function nearest(array $sample) : array
     {
-        $distances = [];
+        $heap = new SplMaxHeap();
 
-        foreach ($this->samples as $neighbor) {
-            $distances[] = $this->kernel->compute($sample, $neighbor);
+        foreach ($this->samples as $index => $neighbor) {
+            $distance = $this->kernel->compute($sample, $neighbor);
+
+            if (is_nan($distance)) {
+                continue;
+            }
+
+            if ($heap->count() < $this->k) {
+                $heap->insert([$distance, $index]);
+
+                continue;
+            }
+
+            if ($distance >= $heap->top()[0]) {
+                continue;
+            }
+
+            $heap->extract();
+
+            $heap->insert([$distance, $index]);
         }
 
-        asort($distances);
+        $labels = $distances = [];
 
-        $distances = array_slice($distances, 0, $this->k, true);
+        foreach ($heap as [$distance, $index]) {
+            $labels[] = $this->labels[$index];
 
-        $labels = array_intersect_key($this->labels, $distances);
+            $distances[] = $distance;
+        }
 
         return [$labels, $distances];
     }
