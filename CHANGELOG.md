df171316b7ed71a3d6eea2a0c88f456d8a8de46c
Andrew DalPino <andrewdalpino@icloud.com>
2026-08-29 18:36:29 -0500

    Add tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,7 @@
     - Added Hard Sigmoid, Hard SiLU, and ReLU6 activation functions
     - Added He Normal, Le Cun Normal, Truncated Normal, Xavier 1 & 2 Normal
     - Cross Entropy loss function now split into Binary and Multiclass
+    - LDA, PCA, SVD now powered by NumPower
     
 - 2.5.10
     - Optimize brute-force nearest neighbors operations
--- tests/Transformers/PrincipalComponentAnalysisTest.php
+++ tests/Transformers/PrincipalComponentAnalysisTest.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
 use Rubix\ML\Datasets\Generators\Blob;
 use Rubix\ML\Transformers\PrincipalComponentAnalysis;
+use Rubix\ML\Exceptions\InvalidArgumentException;
 use Rubix\ML\Exceptions\RuntimeException;
 use PHPUnit\Framework\TestCase;
 
@@ -56,5 +57,61 @@ class PrincipalComponentAnalysisTest extends TestCase
         $samples = $this->generator->generate(1)->samples();
 
         $this->transformer->transform($samples);
+    }
+
+    #[Test]
+    public function badDimensions() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new PrincipalComponentAnalysis(0);
+    }
+
+    #[Test]
+    public function lossiness() : void
+    {
+        $this->assertNull($this->transformer->lossiness());
+
+        $dataset = $this->generator->generate(30);
+
+        $this->transformer->fit($dataset);
+
+        $lossiness = $this->transformer->lossiness();
+
+        $this->assertIsFloat($lossiness);
+        $this->assertGreaterThanOrEqual(0.0, $lossiness);
+        $this->assertLessThanOrEqual(1.0, $lossiness);
+
+        $full = new PrincipalComponentAnalysis(4);
+        $full->fit($dataset);
+
+        $this->assertLessThanOrEqual($lossiness, $full->lossiness());
+    }
+
+    #[Test]
+    public function serializeRoundTrip() : void
+    {
+        $this->transformer->fit($this->generator->generate(30));
+
+        $sample = $this->generator->generate(3)->samples();
+
+        $expected = $sample;
+        $this->transformer->transform($expected);
+
+        $copy = unserialize(serialize($this->transformer));
+
+        $this->assertInstanceOf(PrincipalComponentAnalysis::class, $copy);
+        $this->assertTrue($copy->fitted());
+
+        $actual = $sample;
+        $copy->transform($actual);
+
+        foreach ($actual as $i => $row) {
+            $this->assertCount(count($expected[$i]), $row);
+
+            foreach ($row as $j => $value) {
+                $this->assertEqualsWithDelta($expected[$i][$j], $value, 1e-4);
+            }
+        }
     }
 }
